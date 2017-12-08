55b56f78b0d9a6f3d8bc0fbe2f07f3382179cba8
t-nakamura <kbc14a08@stu.kawahara.ac.jp>
2017-12-08 10:33:13 +0900

    [update] サーバにNode.jsをインストールする

--- StartOfEnvironment.sh
+++ StartOfEnvironment.sh
@@ -6,6 +6,10 @@ set -e
 apt update -y
 apt upgrade -y
 
+#Nodejs 8.9.2
+RUN curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+RUN apt-get install -y nodejs
+
 #fabricの必要なイメージ一覧の取得
 curl -sSL https://goo.gl/5ftp2f | bash
 
