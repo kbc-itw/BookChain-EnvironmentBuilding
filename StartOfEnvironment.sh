8ab0a9fc0048e017cf0815538b20745ca41997d7
t-nakamura <kbc14a08@stu.kawahara.ac.jp>
2017-12-08 10:27:17 +0900

    [update] サーバにサンプルの環境を構築する処理追加

--- StartOfEnvironment.sh
+++ StartOfEnvironment.sh
@@ -9,3 +9,14 @@ apt upgrade -y
 #fabricの必要なイメージ一覧の取得
 curl -sSL https://goo.gl/5ftp2f | bash
 
+#fabric-sample　pull
+cd /opt
+git clone https://github.com/hyperledger/fabric-samples.git
+cd /fabric-samples/fabcar
+
+#サンプルで使うfabric環境を作成
+./startFabric.sh
+npm i
+node enrollAdmin.js
+node registerUser.js
+
