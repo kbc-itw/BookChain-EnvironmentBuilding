a0c7ec343af1c94f7346d327966e74bf084d0c0c
t-nakamura <kbc14a08@stu.kawahara.ac.jp>
2018-01-12 15:21:18 +0900

    [add] docker-composeを使用して必要なfabricの立ち上げ

--- startFabric.sh
+++ startFabric.sh
@@ -3,5 +3,15 @@
 set -ev
 
 # fabricを動作させるのに必要なセッティング
+# fabricがすでにある場合に落とす必要がある
+cd ../fabric-samples/basic-network
+
+configtxgen -profile OneOrgChannel -outputCreateChannelTx ./config/channel.tx -channelID $CHANNEL_NAME
+
+docker-compose -f docker-compose.yml down
+# fabricを立ち上げる
+docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com peer0.org1.example.com couchdb
 
 # fabricを動作させる
+# fabricが立ち上がるまでの待機時間
+sleep 5
