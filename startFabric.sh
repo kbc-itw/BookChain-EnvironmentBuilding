59161a71d975da5eeafbc7b9442ed43fb61549c4
t-nakamura <kbc14a08@stu.kawahara.ac.jp>
2018-01-12 15:28:37 +0900

    [add] fabric-sampls/fabcar/hfc-key-store内の中身を削除

--- startFabric.sh
+++ startFabric.sh
@@ -22,3 +22,6 @@ sleep 5
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f /etc/hyperledger/configtx/channel.tx
 # peer0.org1.example.com にチャンネルをつなぐ
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b $CHANNEL_NAME.block
+
+# userのkeyをリセット
+rm -rf ../fabcar/hfc-key-store
