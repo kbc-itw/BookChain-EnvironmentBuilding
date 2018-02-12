8aca5cd5c31110781512e7daca522a0c185bf960
tomo <vista225kadenzuki@gmail.com>
2018-02-12 21:56:10 +0900

    ファイルパスを追加

--- runPlatform.sh
+++ runPlatform.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+cd ../
+ROOT_PATH=$PWD
+
 docker build -f ./Dockerfile-platform -t platform .
 docker run -d --name platform -v $ROOT_PATH/BookChain-Client:/opt/BookChain -v $ROOT_PATH/BookChain:/opt/BookChain -t -i -p 80:80 -p 8080:8080 platform
