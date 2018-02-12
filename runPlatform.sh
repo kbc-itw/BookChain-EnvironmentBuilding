2d420f5aa8e400786b32c299b3843f0e92f70047
tomo <vista225kadenzuki@gmail.com>
2018-02-13 01:08:16 +0900

    修正

--- runPlatform.sh
+++ runPlatform.sh
@@ -5,5 +5,5 @@ set -ev
 cd ../
 ROOT_PATH=$PWD
 
-docker build -f ./Dockerfile-platform -t platform .
-docker run -d --name platform -v $ROOT_PATH/BookChain-Client:/opt/BookChain-Client -v $ROOT_PATH/BookChain:/opt/BookChain -t -i -p 80:80 -p 8080:8080 platform
+docker build -f ./BookChain-EnvironmentBuilding/Dockerfile-platform -t platform .
+docker run -d --name platform -v $ROOT_PATH/BookChain-Client:/opt/BookChain-Client -v $ROOT_PATH/BookChain:/opt/BookChain -t -i -p 80:80 -p 8080:8080 --link couchdb:couchdb --network=net_basic platform
