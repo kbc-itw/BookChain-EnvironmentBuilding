df38d96cbd56e9b7c94b68ad11a16ee583ab88a2
tomo <vista225kadenzuki@gmail.com>
2018-02-12 22:35:33 +0900

    パスの修正

--- runPlatform.sh
+++ runPlatform.sh
@@ -4,4 +4,4 @@ cd ../
 ROOT_PATH=$PWD
 
 docker build -f ./Dockerfile-platform -t platform .
-docker run -d --name platform -v $ROOT_PATH/BookChain-Client:/opt/BookChain -v $ROOT_PATH/BookChain:/opt/BookChain -t -i -p 80:80 -p 8080:8080 platform
+docker run -d --name platform -v $ROOT_PATH/BookChain-Client:/opt/BookChain-Client -v $ROOT_PATH/BookChain:/opt/BookChain -t -i -p 80:80 -p 8080:8080 platform
